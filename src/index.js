976ad673d57f5a4e56b6a3ba952c82dd0962cea9
lostandfound <strawberria1023@gmail.com>
2017-03-18 02:21:18 +0900

    install Eslint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 "use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 const kuromojin = require("kuromojin");
 const createMatcher = require("morpheme-match-all");
-const yaml = require('js-yaml');
-const data = yaml.safeLoad(fs.readFileSync(__dirname + "/../dict/hojodoushi.yml", 'utf8'));
+const yaml = require("js-yaml");
+const data = yaml.safeLoad(fs.readFileSync(__dirname + "/../dict/hojodoushi.yml", "utf8"));
 
-let dictionaries = [];
+const dictionaries = [];
 
-data.dict.forEach(function (item) {
+data.dict.forEach(function(item) {
   var form = "";
-  item.tokens.forEach(function (token) {
+  item.tokens.forEach(function(token) {
     form += token.surface_form;
   });
   dictionaries.push({
@@ -23,39 +23,45 @@ data.dict.forEach(function (item) {
 const matchAll = createMatcher(dictionaries);
 
 function reporter(context, options = {}) {
-    const {Syntax, RuleError, report, getSource, fixer} = context;
-    return {
-      [Syntax.Str](node){ // "Str" node
-        const text = getSource(node); // Get text
-        return kuromojin(text).then((actualTokens) => {
-          const results = matchAll(actualTokens);
+  const {
+    Syntax,
+    RuleError,
+    report,
+    getSource,
+    fixer
+  } = context;
+  return {
+    [Syntax.Str](node) { // "Str" node
+      const text = getSource(node); // Get text
+      return kuromojin(text).then((actualTokens) => {
+        const results = matchAll(actualTokens);
 
-          if (results.length == 0) {
-            return;
-          }
+        if (results.length == 0) {
+          return;
+        }
 
-          results.forEach(function (result) {
-            let tokenIndex = result.index;
-            let index = getIndexFromTokens(tokenIndex, actualTokens);
-            let replaceFrom = "";
-            result.tokens.forEach(function(token){
-              replaceFrom += token.surface_form;
-            });
-            let replaceTo = fixer.replaceTextRange([index, index + replaceFrom.length], result.dict.fix);
-            let ruleError = new RuleError(result.dict.message, {
-              index: index,
-              fix:   replaceTo // https://github.com/textlint/textlint/blob/master/docs/rule-fixable.md
-            });
-            report(node, ruleError);
+        results.forEach(function(result) {
+          const tokenIndex = result.index;
+          const index = getIndexFromTokens(tokenIndex, actualTokens);
+          let replaceFrom = "";
+          result.tokens.forEach(function(token) {
+            replaceFrom += token.surface_form;
           });
+          const replaceTo = fixer.replaceTextRange([index, index + replaceFrom.length], result.dict.fix);
+          const ruleError = new RuleError(result.dict.message, {
+            index: index,
+            fix: replaceTo // https://github.com/textlint/textlint/blob/master/docs/rule-fixable.md
+          });
+          report(node, ruleError);
         });
-      }
+      });
     }
-};
+  };
+}
 
 function getIndexFromTokens(tokenIndex, actualTokens) {
   let index = 0;
-  for ( let i = 0; i < tokenIndex; i++) {
+  for (let i = 0; i < tokenIndex; i++) {
     index += actualTokens[i].surface_form.length;
   }
   return index;
@@ -65,4 +71,3 @@ module.exports = {
   linter: reporter,
   fixer: reporter
 };
-
